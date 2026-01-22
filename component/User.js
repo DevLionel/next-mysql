1c412ba893dd69ef36de234a14fa5a3702651f4f
Lionel <lionelhuizing@gmail.com>
2026-01-22 15:53:46 +0100

    added components

---
+++ component/User.js
@@ -0,0 +1,24 @@
+function User(){
+
+    return(
+        <>
+            <tr>
+                <input type="hidden" id="userId" name="id" value = "" />
+				<td>
+				    		<span class="custom-checkbox">
+					    <input type="checkbox" id="data_checkbox" class="data_checkbox" name="data_checkbox" value=""/>
+		    	    	<label for="data_checkbox"></label>
+			    	</span>
+				</td>
+    				<td>Anand Raj</td>
+	    			<td>Rajanand@gmail.com</td>
+				<td>
+					<a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+				    <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+			    </td>
+			</tr>
+        </>
+    )
+}
+
+export default User;
