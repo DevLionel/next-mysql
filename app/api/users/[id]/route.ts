eb7e27cafbe7f0d694e31d007a2f832337a066fd
Lionel <lionelhuizing@gmail.com>
2026-02-16 11:32:17 +0100

    Added delete functionality

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -29,3 +29,32 @@ export async function GET(req: NextRequest, context: { params: Promise<{ id: str
  
     return NextResponse.json({ user, success: true }, { status: 200 });
 }
+
+export async function DELETE(req: NextRequest, context: { params: Promise<{ id: string}> }) {
+        
+    const { id } = await context.params;
+
+    const userId = Number(id);
+
+    if(isNaN(userId))
+    {
+      return NextResponse.json(
+        { message: "Invalid ID", id },
+        { status: 400 }
+        );
+    }
+    
+    const user = await prisma.user.delete({
+        where : { id : userId }
+    })
+
+    if(!user)
+    {
+        return NextResponse.json(
+            { message: "User not found" },
+            { status: 404 }
+        )    
+    }
+ 
+    return NextResponse.json({ user, success: true }, { status: 200 });
+}
