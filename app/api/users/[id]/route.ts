01abf0fc20abbfb44d2520d4c994f3e65ca116dc
Lionel <lionelhuizing@gmail.com>
2026-03-10 13:42:29 +0100

    Changed behavior PUT function in route.ts

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -2,9 +2,9 @@ import { prisma } from "../../../../lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 
-export async function GET(req: NextRequest, context: { params: Promise<{ id: string}> }) {
+export async function GET(req: NextRequest, {params}: { params: Promise<{ id: string}> }) {
         
-    const { id } = await context.params;
+    const { id } = await params;
 
     const userId = Number(id);
 
